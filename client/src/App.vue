8d5bb900223afa7d0d963a9346877c057533c6a4
JenkenK <jenkenkwok@gmail.com>
2020-11-01 15:25:42 +0000

    api started but not working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
     <h1>BlackJack</h1>
+    <div>{{ drawCards(this.deck_id, 2) }}</div>
+    <div>{{ this.deck_id }}</div>
+    <div>{{ this.player.hand }}</div>
   </div>
 </template>
 
 <script>
-export default {};
+import CardsAPI from "./api/CardsAPI.js";
+
+export default {
+  data() {
+    return {
+      deck_id: "",
+      player: {
+        hand: [],
+      },
+    };
+  },
+
+  mounted() {
+    CardsAPI.shuffleNewDeck().then((res) => {
+      this.deck_id = res.deck_id;
+    });
+  },
+
+  methods: {
+    drawCards() {
+      CardsAPI.draw(this.deck_id, 2).then((res) => {
+        res.cards.forEach((card) => {
+          this.player.hand.push(card);
+        });
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
