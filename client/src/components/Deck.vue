764b46d13e172f9d0fe379ee73662f3619702419
JenkenK <jenkenkwok@gmail.com>
2020-11-01 20:17:52 +0000

    added draw card function for player

--- client/src/components/Deck.vue
+++ client/src/components/Deck.vue
@@ -1,8 +1,49 @@
-<template><div id="deck"></div></template>
+<template>
+  <div id="deck">
+    <h2>Deck</h2>
+    <div>Deck ID: {{ this.deck_id }}</div>
+    <br />
+    <button v-on:click="drawCards">Draw Cards</button>
+  </div>
+</template>
 
 <script>
-export default {};
+import { eventBus } from "../main.js";
+import CardsAPI from "../api/CardsAPI";
+
+export default {
+  data() {
+    return {
+      deck: [],
+      deck_id: "",
+      player: {
+        hand: [],
+        cardImgs: [],
+      },
+    };
+  },
+  mounted() {
+    CardsAPI.shuffleNewDeck().then((res) => {
+      this.deck_id = res.deck_id;
+    });
+  },
+  methods: {
+    drawCards() {
+      CardsAPI.draw(this.deck_id, 2).then((res) => {
+        console.log(res);
+        res.cards.forEach((card) => {
+          this.player.hand.push(card);
+          this.player.cardImgs.push(card.image);
+        });
+        eventBus.$emit("player", this.player);
+      });
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+#id {
+  border: 2px solid black;
+}
 </style>
--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -1,7 +1,32 @@
-<template><div id="player"></div></template>
+<template>
+  <div id="player">
+    <h2>Player</h2>
+    <div>{{ this.player.hand }}</div>
+    <div>{{ this.player.cardImgs }}</div>
+    <img :src="player.cardImgs[0]" alt="" />
+    <img :src="player.cardImgs[1]" alt="" />
+  </div>
+</template>
 
 <script>
-export default {};
+import { eventBus } from "../main.js";
+export default {
+  name: "player",
+  //   props: ["player"],
+  data() {
+    return {
+      player: {
+        cardImgs: [],
+        hand: [],
+      },
+    };
+  },
+  mounted() {
+    eventBus.$on("player", (data) => {
+      this.player = data;
+    });
+  },
+};
 </script>
 
 <style>
